a943f163cb75e388956ec32a301940957468f7de
Mikhail Proniushkin <mikhailpro4@gmail.com>
2017-12-20 19:24:14 -0500

    more styling + postcss autoprefixer

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -12,7 +12,7 @@ module.exports = {
 				test: /\.scss$/,
 				use: ExtractTextPlugin.extract({
 					fallback: "style-loader",
-					use: ["css-loader", "sass-loader"]
+					use: ["css-loader", "postcss-loader", "sass-loader"]
 				})
 			},
 			{ test: /\.json$/, loader: "json-loader" },
